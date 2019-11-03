58600bd687b322f8c7e7f2220e4c6136ce904424
ReynaDuong <minhduong.md25@gmail.com>
2019-11-03 00:42:50 -0500

    create controller and link to view

--- src/main/java/com/minhduong/web/application/ReservationController.java
+++ src/main/java/com/minhduong/web/application/ReservationController.java
@@ -1,15 +1,45 @@
 package com.minhduong.web.application;
 
+import com.minhduong.business.domain.RoomReservation;
+import com.minhduong.business.service.ReservationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
 @Controller
 @RequestMapping(value = "/reservations")
 public class ReservationController {
 
+    private static final DateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
+
+    @Autowired
+    private ReservationService reservationService;
+
     @RequestMapping(method = RequestMethod.GET)
-    public String getReservation() {
+    public String getReservation(@RequestParam(value = "date", required = false) String dateString, Model model) {
+        Date date = null;
+        if (dateString != null) {
+            try {
+                date = DATE_FORMAT.parse(dateString);
+            }
+            catch (ParseException e) {
+                date = new Date();
+            }
+        }
+        else {
+            date = new Date();
+        }
+        List<RoomReservation> roomReservationsList = reservationService.getRoomReservationsForDate(date);
+        model.addAttribute("roomReservations", roomReservationsList);
         return "reservations";
     }
 }
