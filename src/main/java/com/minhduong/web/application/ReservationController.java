1af5b71642954183c3112a67a431e625c561e88e
ReynaDuong <minhduong.md25@gmail.com>
2019-11-05 10:03:37 -0600

    reformat code

--- src/main/java/com/minhduong/web/application/ReservationController.java
+++ src/main/java/com/minhduong/web/application/ReservationController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.text.DateFormat;
 import java.text.ParseException;
@@ -15,7 +16,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping(value = "/reservations")
 public class ReservationController {
 
--- src/main/java/com/minhduong/web/application/RoomController.java
+++ src/main/java/com/minhduong/web/application/RoomController.java
@@ -13,6 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping(value = "/rooms")
 public class RoomController {
 
     @Autowired
