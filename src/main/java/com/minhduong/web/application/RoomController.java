bed62a96159d1b52ef3696cba5df6d3bef377c96
ReynaDuong <minhduong.md25@gmail.com>
2019-12-18 16:22:19 -0600

    clean up Dependencies

--- src/main/java/com/minhduong/web/application/RoomController.java
+++ src/main/java/com/minhduong/web/application/RoomController.java
@@ -1,4 +1,11 @@
 package com.minhduong.web.application;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/rooms")
 public class RoomController {
+
+
 }
